76b9073e6d825bf8e1444332650f326a7062ac39
Sunnik Chatterjee <sunnikchatterjee21@gmail.com>
2026-05-08 21:39:17 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length())
+            return "";
+        Map<Character, Integer> count1 = new HashMap<>();
+        Map<Character, Integer> count2 = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            count2.put(t.charAt(i), count2.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        int need = count2.size();
+        int have = 0, l = 0;
+        int startIndex = 0, minLen = Integer.MAX_VALUE;
+        for (int r = 0; r < s.length(); r++) {
+            count1.put(s.charAt(r), count1.getOrDefault(s.charAt(r), 0) + 1);
+            if (count2.containsKey(s.charAt(r))
+                && count1.get(s.charAt(r)).equals(count2.get(s.charAt(r))))
+                have++;
+            while(have==need){
+                if((r-l)+1<minLen){
+                    minLen=(r-l)+1;
+                    startIndex = l;
+                }
+                count1.put(s.charAt(l),count1.getOrDefault(s.charAt(l),0)-1);
+                if(count2.containsKey(s.charAt(l)) && count1.get(s.charAt(l))<count2.get(s.charAt(l))) have--;
+                l++;
+            }
+        }
+        if(minLen == Integer.MAX_VALUE) return "";
+        return s.substring(startIndex,startIndex+minLen);
+    }
+}
