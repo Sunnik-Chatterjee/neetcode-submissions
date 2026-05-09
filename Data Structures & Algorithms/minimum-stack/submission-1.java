0498233030c381937caf0d3dc42325f677240038
Sunnik Chatterjee <sunnikchatterjee21@gmail.com>
2026-05-09 13:12:15 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,31 @@
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> minSt;
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push(val);
+            minSt.push(val);
+        }else{
+            st.push(val);
+            minSt.push(Math.min(minSt.peek(),val));
+        }
+    }
+    
+    public void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return minSt.peek();
+    }
+}
