32fe20f52dd0accefa60b082cdb8844ceefb8f3b
Sunnik Chatterjee <sunnikchatterjee21@gmail.com>
2026-05-14 12:42:50 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s:strs){
+            int len = s.length();
+            sb.append(len).append("#").append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> ans = new ArrayList<>();
+        int i=0;
+        while(i<str.length()){
+            int delimiter = str.indexOf("#",i);
+            int len = Integer.parseInt(str.substring(i,delimiter));
+            i=delimiter+1;
+            String s = str.substring(i,i+len);
+            i+=len;
+            ans.add(s);
+        }
+        return ans;
+    }
+}
