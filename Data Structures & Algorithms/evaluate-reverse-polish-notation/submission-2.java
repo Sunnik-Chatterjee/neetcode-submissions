5ba39666f3915bc2f98ce523f6b2ba4cfb382c5d
Sunnik Chatterjee <sunnikchatterjee21@gmail.com>
2026-05-16 12:32:35 +0530

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for (String s : tokens) {
+            if (s.matches("-?\\d+"))
+                st.push(Integer.parseInt(s));
+            else {
+                int num1, num2;
+                switch (s) {
+                    case "+":
+                        num2 = st.peek();
+                        st.pop();
+                        num1 = st.peek();
+                        st.pop();
+                        st.push(num1 + num2);
+                        break;
+                    case "-":
+                        num2 = st.peek();
+                        st.pop();
+                        num1 = st.peek();
+                        st.pop();
+                        st.push(num1 - num2);
+                        break;
+                    case "*":
+                        num2 = st.peek();
+                        st.pop();
+                        num1 = st.peek();
+                        st.pop();
+                        st.push(num1 * num2);
+                        break;
+                    case "/":
+                        num2 = st.peek();
+                        st.pop();
+                        if (num2 != 0 && !st.isEmpty()) {
+                            num1 = st.peek();
+                            st.pop();
+                            st.push(num1 / num2);
+                        }
+                }
+            }
+        }
+        if(!st.isEmpty()) return st.peek();
+        else return 0;
+    }
+}
